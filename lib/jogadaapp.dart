06715559ee3b336c37c5a12783a8b9f1717bf3eb
amarcdev <amarcdev@gmail.com>
2021-12-02 21:35:00 -0300

    Teste primeira jogada

--- lib/jogadaapp.dart
+++ lib/jogadaapp.dart
@@ -2,9 +2,46 @@ import 'package:crosscircle/single.dart';
 import 'package:flutter/material.dart';
 import 'rules.dart';
 
+var trinc1 = [0, 1, 2];
+var trinc2 = [0, 3, 6];
+var trinc3 = [3, 4, 5];
+var trinc4 = [1, 4, 7];
+var trinc5 = [6, 7, 8];
+var trinc6 = [2, 5, 8];
+var trinc7 = [0, 4, 8];
+var trinc8 = [2, 4, 6];
+
+int resultapp = 5;
+
+testar() {
+  for (a = 0;a<9;a++){
+    if (cels[a]=="B"){
+      for (int b = 0; b < 3; b++){
+        if (a == trinc1[b]){
+          for(int c=0;c<trinc1.length;c++){
+            if(cels[trinc1[c]]=="I"){
+              if(a>trinc1[c]){
+                int d = c;
+                resultapp = trinc1[d];
+                return resultapp;
+              }else if(a<trinc1[c]){
+                int d = c;
+                resultapp = trinc1[d]
+                print(maior);
+                a=9;
+                return resultapp;
+              }
+            }
+          }
+        }
+      }
+    }else{}
+  }
+  
+}
+
 Future jogadaapp() async {
-  int escolhaapp = 0, t = 0, u = 0, i = 0;
-  int encontrou = 0;
+  int escolhaapp = 0, encontrou = 0, t = 0, u = 0, i = 0;
   for (i = 0; i < cels.length; i++) {
     if (cels[i] == "B" && encontrou == 0) {
       for (t = i; t < cels.length; t++) {
@@ -14,8 +51,7 @@ Future jogadaapp() async {
           encontrou = 1;
           i = cels.length;
           cels[escolhaapp] = "B";
-          bkcel[escolhaapp] =
-              MaterialStateProperty.all(Colors.brown);
+          bkcel[escolhaapp] = MaterialStateProperty.all(Colors.brown);
           icon[escolhaapp] = Icon(Icons.android, size: 80);
           return escolhaapp;
         }
@@ -28,8 +64,7 @@ Future jogadaapp() async {
           encontrou = 1;
           i = cels.length;
           cels[escolhaapp] = "B";
-          bkcel[escolhaapp] =
-              MaterialStateProperty.all(Colors.brown);
+          bkcel[escolhaapp] = MaterialStateProperty.all(Colors.brown);
           icon[escolhaapp] = Icon(Icons.android, size: 80);
           return escolhaapp;
         }
@@ -46,8 +81,7 @@ Future jogadaapp() async {
             encontrou = 1;
             i = cels.length;
             cels[escolhaapp] = "B";
-            bkcel[escolhaapp] =
-                MaterialStateProperty.all(Colors.brown);
+            bkcel[escolhaapp] = MaterialStateProperty.all(Colors.brown);
             icon[escolhaapp] = Icon(Icons.android, size: 80);
             return escolhaapp;
           }
@@ -60,8 +94,7 @@ Future jogadaapp() async {
             encontrou = 1;
             i = cels.length;
             cels[escolhaapp] = "B";
-            bkcel[escolhaapp] =
-                MaterialStateProperty.all(Colors.brown);
+            bkcel[escolhaapp] = MaterialStateProperty.all(Colors.brown);
             icon[escolhaapp] = Icon(Icons.android, size: 80);
             return escolhaapp;
           }
